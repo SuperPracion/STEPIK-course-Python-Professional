cdc3424e5718dba0d09e8889bbacd7ea1e680fe0
Pracion <59648797+SuperPracion@users.noreply.github.com>
2022-10-29 14:00:57 +0300

    Delete 1.py

--- Section 4.4/1.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-open, closed = "09:00-12:00".split('-')
-
-print(datetime.strptime(open, '%H:%M') <= datetime.strptime("10:00", '%H:%M') and
-      datetime.strptime(closed, '%H:%M') >= datetime.strptime("12:00", '%H:%M'))
